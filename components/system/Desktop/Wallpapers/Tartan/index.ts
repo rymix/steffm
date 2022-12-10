62dded06f0ed9303872383441cf086695946e540
steveasmyle <steve.a@smyle.co.uk>
2022-12-10 17:32:33 +0000

    Disabled pre-commit for now

--- components/system/Desktop/Wallpapers/Tartan/index.ts
+++ components/system/Desktop/Wallpapers/Tartan/index.ts
@@ -1,4 +1,4 @@
-const libs = ["/System/Matrix/js/regl/main.js"];
+// const libs = ["/System/Matrix/js/regl/main.js"];
 
 declare global {
   interface Window {
@@ -6,22 +6,27 @@ declare global {
   }
 }
 
-const Tartan = async (el?: HTMLElement | null): Promise<void> => {
-  if (!el) return;
+// const Tartan = (el?: HTMLElement | null): Promise<void> => {
+//   if (!el) return;
 
-  const canvas = document.createElement("canvas");
+//   const canvas = document.createElement("canvas");
 
-  canvas.height = window.innerHeight;
-  canvas.width = window.innerWidth;
+//   canvas.height = window.innerHeight;
+//   canvas.width = window.innerWidth;
 
-  const canvasContext = canvas.getContext("2d");
-  // canvasContext.fillStyle = "blue";
+//   // const canvasContext = canvas.getContext("2d");
+//   // canvasContext.fillStyle = "blue";
 
-  el.append(canvas);
+//   el.append(canvas);
 
-  // await loadFiles(libs, undefined, undefined, true);
+//   // await loadFiles(libs, undefined, undefined, true);
 
-  // await window.Tartan?.(canvas, { ...matrixConfig, ...config });
+//   // await window.Tartan?.(canvas, { ...matrixConfig, ...config });
+// };
+
+const Tartan = (el?: HTMLElement | null): void => {
+  // eslint-disable-next-line no-console
+  console.log("hello", el);
 };
 
 export default Tartan;
