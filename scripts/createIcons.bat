c83323ae269bd3ca597ab9d3957fc051c52bba43
steveasmyle <steve.a@smyle.co.uk>
2022-12-14 09:17:35 +0000

    Added 24x24 icon support

--- scripts/createIcons.bat
+++ scripts/createIcons.bat
@@ -4,6 +4,7 @@ MKDIR "..\144x144"
 MKDIR "..\96x96"
 MKDIR "..\48x48"
 MKDIR "..\32x32"
+MKDIR "..\24x24"
 MKDIR "..\16x16"
 
 FOR %%X in (*.ico) DO (
@@ -11,6 +12,7 @@ FOR %%X in (*.ico) DO (
     C:\ImageMagick\magick.exe "%%X[0]" -background none -compress lossless -scale 96x96 -strip -quality 100 "..\96x96\%%~nX.png"
     C:\ImageMagick\magick.exe "%%X[2]" -background none -compress lossless -strip -quality 100 "..\48x48\%%~nX.png"
     C:\ImageMagick\magick.exe "%%X[4]" -background none -compress lossless -strip -quality 100 "..\32x32\%%~nX.png"
+    C:\ImageMagick\magick.exe "%%X[6]" -background none -compress lossless -strip -quality 100 "..\24x24\%%~nX.png"
     C:\ImageMagick\magick.exe "%%X[7]" -background none -compress lossless -strip -quality 100 "..\16x16\%%~nX.png"
 
     C:\ImageMagick\magick.exe "%%X[0]" -background none -compress lossless -strip -quality 100 "..\max\%%~nX.png"
@@ -19,6 +21,7 @@ FOR %%X in (*.ico) DO (
     cwebp -lossless -m 6 -z 9 -q 100 -alpha_q 100 -sharp_yuv -resize 96 0 -mt -alpha_filter best -v "..\max\%%~nX.png" -o "..\96x96\%%~nX.webp"
     cwebp -lossless -m 6 -z 9 -q 100 -alpha_q 100 -sharp_yuv -mt -alpha_filter best -v "..\48x48\%%~nX.png" -o "..\48x48\%%~nX.webp"
     cwebp -lossless -m 6 -z 9 -q 100 -alpha_q 100 -sharp_yuv -mt -alpha_filter best -v "..\32x32\%%~nX.png" -o "..\32x32\%%~nX.webp"
+    cwebp -lossless -m 6 -z 9 -q 100 -alpha_q 100 -sharp_yuv -mt -alpha_filter best -v "..\24x24\%%~nX.png" -o "..\24x24\%%~nX.webp"
     cwebp -lossless -m 6 -z 9 -q 100 -alpha_q 100 -sharp_yuv -mt -alpha_filter best -v "..\16x16\%%~nX.png" -o "..\16x16\%%~nX.webp"
 )
 
@@ -53,6 +56,14 @@ FOR /D %%X in (*) DO (
     ) ELSE (
         COPY "%%X\32x32.png" "..\32x32\%%~nX.png"
         cwebp -lossless -m 6 -z 9 -q 100 -alpha_q 100 -sharp_yuv -mt -alpha_filter best -v "..\32x32\%%~nX.png" -o "..\32x32\%%~nX.webp"
+    )
+
+    IF NOT EXIST "%%X\24x24.png" (
+        C:\ImageMagick\magick.exe "%%X\max.png" -compress lossless -scale 24x24 -strip -quality 100 "..\24x24\%%~nX.png"
+        cwebp -lossless -m 6 -z 9 -q 100 -alpha_q 100 -sharp_yuv -resize 24 24 -mt -alpha_filter best -v "%%X\max.png" -o "..\24x24\%%~nX.webp"
+    ) ELSE (
+        COPY "%%X\24x24.png" "..\24x24\%%~nX.png"
+        cwebp -lossless -m 6 -z 9 -q 100 -alpha_q 100 -sharp_yuv -mt -alpha_filter best -v "..\24x24\%%~nX.png" -o "..\24x24\%%~nX.webp"
     )
 
     IF NOT EXIST "%%X\16x16.png" (
