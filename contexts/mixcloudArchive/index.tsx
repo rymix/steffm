277566ac657975e71d34d0b424d679902b866042
Steve Arnott <steve.a@smyle.co.uk>
2022-12-20 10:58:00 +0000

    Deleted mixcloud archive context

--- contexts/mixcloudArchive/index.tsx
+++
@@ -1,6 +0,0 @@
-import contextFactory from "contexts/contextFactory";
-import useMixcloudContextState from "contexts/mixcloudArchive/useMixcloudContextState";
-
-const { Provider, useContext } = contextFactory(useMixcloudContextState);
-
-export { Provider as MixcloudProvider, useContext as useMixcloudArchive };
