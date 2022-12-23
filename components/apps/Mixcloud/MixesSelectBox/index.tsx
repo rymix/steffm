c44558f6fcb6bd8e9b8e045585fd10a6b5dc3879
Steve Arnott <steve.a@smyle.co.uk>
2022-12-23 10:57:01 +0000

    Added filtered category lists

--- components/apps/Mixcloud/MixesSelectBox/index.tsx
+++ components/apps/Mixcloud/MixesSelectBox/index.tsx
@@ -2,13 +2,22 @@ import StyledMixesSelectBox from "components/apps/Mixcloud/MixesSelectBox/Styled
 import { useMixcloud } from "contexts/mixcloud";
 
 const MixesSelecBox = (): JSX.Element => {
-  const { mixes, mixcloudKey, setMixcloudKey } = useMixcloud();
-
-  return (
-    <StyledMixesSelectBox onChange={(e) => setMixcloudKey(e.target.value)}>
-      <option value="Select">Select</option>
-
-      {mixes.map((mix) => (
+  const { mixes, mixcloudKey, selectedCategory, setMixcloudKey } =
+    useMixcloud();
+  const showAllCategories = () =>
+    mixes.map((mix) => (
+      <option
+        key={mix.mixcloudKey}
+        selected={mix.mixcloudKey === mixcloudKey}
+        value={mix.mixcloudKey}
+      >
+        {mix.name}
+      </option>
+    ));
+  const showFilteredCategories = () =>
+    mixes
+      .filter((mix) => mix.category === selectedCategory)
+      .map((mix) => (
         <option
           key={mix.mixcloudKey}
           selected={mix.mixcloudKey === mixcloudKey}
@@ -16,7 +25,15 @@ const MixesSelecBox = (): JSX.Element => {
         >
           {mix.name}
         </option>
-      ))}
+      ));
+
+  return (
+    <StyledMixesSelectBox onChange={(e) => setMixcloudKey(e.target.value)}>
+      <option value="Select">Select</option>
+
+      {selectedCategory === "all"
+        ? showAllCategories()
+        : showFilteredCategories()}
     </StyledMixesSelectBox>
   );
 };
